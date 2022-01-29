fd2d082f4cd257057187f40405bcee9f2ce3c618
VartanyanE <mike.vartanyan@yahoo.com>
2022-01-29 10:05:02 -0800

    front end API

---
+++ client/src/utils/Api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const getData = function () {
+  return axios.get("/api/data");
+};
+
+export const upDateData = function (id, data) {
+   return axios.put("/api/data/" + id, data);
+ };
+
+export const postData = function (data) {
+    return axios.post("/api/data" + data);
+}
+
+ export const deleteData = function (id) {
+   return axios.delete("/api/data/" + id);
+ };
