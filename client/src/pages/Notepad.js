db565c7e82c07f113a97a2d064fd1721f19ae3ad
VartanyanE <mike.vartanyan@yahoo.com>
2022-01-30 10:04:15 -0800

    css grid layout

--- client/src/pages/Notepad.js
+++ client/src/pages/Notepad.js
@@ -16,22 +16,45 @@ const Notepad = () => {
     return (
       <div className="container">
         <div className="mainTitle">Notepad Application </div>
-        <div className="card">
+         <div className="card">
           <div className="notepad-title-input">
+            
+            Notepad Title <br/>
             <input
-              
               placeholder="My notepad title..."
             ></input>
           </div>
+         
           <div className="view-stats">
             <button className="view-stats-btn">View Stats</button>
           </div>
+          
           <div className="save">
             <button className="save-btn">Save</button>
           </div>
+          
           <div className="delete">
             <button className="delete-btn">Delete</button>
           </div>
+          
+          <div className="notes-heading"> 
+          {  fetchedData.map((item) => (
+               item.notepadTitle 
+      ))}
+           <br/>
+          <input className="note-title-input"
+              placeholder="Enter note title..."
+            ></input>
+            <br/>
+            <input className="note-input"
+              placeholder="Enter note..."
+            ></input>
+          </div>
+          
+          <div className="add">
+            <button className="add-btn">Add</button>
+          </div>
+         
         </div>
       </div>
     );
