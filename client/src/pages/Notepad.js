1405ede0ba90a5f21a0a84f95875c01613dd10f9
VartanyanE <mike.vartanyan@yahoo.com>
2022-01-31 22:59:16 -0800

    wired up the buttons :)

--- client/src/pages/Notepad.js
+++ client/src/pages/Notepad.js
@@ -3,26 +3,34 @@ import React, { useEffect, useState } from "react";
 import "../styles/notepad.css";
 
 const Notepad = () => {
-    const [fetchedData, setFetchedData] = useState([{}]);
-    const [notepadTitle, setNotepadTitle] = useState("");
+  
+    const [notepadTitle, setNotepadTitle] = useState();
+    const [persistnotePadTitle, setPersistnotePadTitle] = useState("")
     const [noteTitle, setNoteTitle] = useState("");
+    const [persistNoteTitle, setPersistNoteTitle] = useState("")
     const [note, setNote] = useState("");
+    const [persistNote, setPersistNote] = useState("");
 
 
 
-  useEffect(() => {
-   
-  }, []);
+//   useEffect(() => {
+//        const theme = localStorage.getItem('notepadTitle') === "true";
+//     const parsedData = JSON.parse(theme);
+//           console.log(parsedData)
+//   }, []);
+    
+  
     
     
-    console.log(fetchedData);
+    console.log(notepadTitle);
    
 
 
 
     const submitNotepadTitle = async (event) => {
         event.preventDefault();
-        localStorage.setItem("notepadTitle", notepadTitle);
+        await setPersistnotePadTitle(notepadTitle)
+        await  localStorage.setItem("notepadTitle", notepadTitle);
         
         await setNotepadTitle("");
 
@@ -30,12 +38,14 @@ const Notepad = () => {
       }
 
       const submitNote = async (event) => {
-        event.preventDefault();
-        localStorage.setItem("noteTitle", noteTitle);
-         localStorage.setItem("note", note);
+          event.preventDefault();
+          await setPersistNoteTitle(noteTitle);
+          await setPersistNote(note);
+          await localStorage.setItem("noteTitle", noteTitle);
+          await localStorage.setItem("note", note);
 
-         await setNoteTitle("");
-         await setNote("");
+          await setNoteTitle("");
+          await setNote("");
 
       }
 
@@ -47,50 +57,67 @@ const Notepad = () => {
           <div className="notepad-title">
             <p>Notepad Title </p>
             <br />
-            
-            <input className="input-title" placeholder="My notepad title..." 
-            value={notepadTitle}
-            onChange={(e) =>
-              setNotepadTitle(e.target.value )
-            } />
+
+            <input
+              className="input-title"
+              placeholder="My notepad title..."
+              value={notepadTitle}
+              onChange={(e) => setNotepadTitle(e.target.value)}
+            />
           </div>
           <div className="title-btns">
             <button className="stats-btn">View Stats</button>
-            <button className="save-btn"
-           onClick={submitNotepadTitle}
-            
-            >Save</button>
-            <button className="del-btn">Delete</button>
+            <button className="save-btn" onClick={submitNotepadTitle}>
+              Save
+            </button>
+            <button className="del-btn" onClick={() => setPersistnotePadTitle("")}>Delete</button>
           </div>
           <div className="note-title">
-           <h3>My Notes</h3>  <br />
-            <input className="input-title" placeholder="Enter Note Title..."
-            value={noteTitle}
-            onChange={(e) =>
-              setNoteTitle(e.target.value )
-            } />
+            {persistnotePadTitle ? persistnotePadTitle : <h3>My Notes</h3>}
+            <br />
+            <input
+              className="input-title"
+              placeholder="Enter Note Title..."
+              value={noteTitle}
+              onChange={(e) => setNoteTitle(e.target.value)}
+            />
           </div>
           <div className="note">
-            <input className="input-note" placeholder="Enter Note..."
-            value={note}
-            onChange={(e) =>
-              setNote(e.target.value)
-            } />
+            <input
+              className="input-note"
+              placeholder="Enter Note..."
+              value={note}
+              onChange={(e) => setNote(e.target.value)}
+            />
           </div>
           <div className="add-btn">
-            <button className="add-btn"
-            onClick={submitNote}
-            >Add</button>
+            <button className="add-btn" onClick={submitNote}>
+              Add
+            </button>
           </div>
+
           <div className="first-note-title">
-            <input className="input-title" />
+            {persistNoteTitle ? (
+              <input className="input-title" placeholder={persistNoteTitle} />
+            ) : (
+              ""
+            )}
           </div>
           <div className="delete-btn">
-            <button className="del-btn">Delete</button>
+            {persistNoteTitle ? (
+              <button className="del-btn" onClick={() => setPersistNoteTitle("")}>Delete</button>
+            ) : (
+              ""
+            )}
           </div>
           <div className="first-note">
-            <input className="input-note"  />
+            {persistNoteTitle ? (
+              <input className="input-note" placeholder={persistNote} />
+            ) : (
+              ""
+            )}
           </div>
+
           {/* <div className="notepad-title-input">
             Notepad Title <br />
                     <input placeholder="My notepad title..."
