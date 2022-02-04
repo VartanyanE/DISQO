c8f3dae837bdbb557ba22568321fab6d5ee6257c
VartanyanE <mike.vartanyan@yahoo.com>
2022-02-03 22:31:59 -0800

    made gist page responsive && persisted notapad data using local storage

--- client/src/pages/Gists.js
+++ client/src/pages/Gists.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import {Link} from "react-router-dom";
-import { Line, CatergoryScale } from 'react-chartjs-2';
+import { Link } from "react-router-dom";
+import { Line } from "react-chartjs-2";
 import "../styles/gists.css";
 import {
   Chart as ChartJS,
@@ -11,9 +11,9 @@ import {
   Title,
   Tooltip,
   Legend,
-  } from 'chart.js';
-  
-  ChartJS.register(
+} from "chart.js";
+
+ChartJS.register(
   CategoryScale,
   LinearScale,
   PointElement,
@@ -21,101 +21,109 @@ import {
   Title,
   Tooltip,
   Legend
-  );
+);
 
 function Gists() {
-
-  
-  const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}]);
   const [createdAtState, setCreatedAtState] = useState([]);
   const [numberOfGists, setNumberOfGists] = useState([]);
 
-     useEffect(() => {
-      fetch('https://api.github.com/gists?per_page=25', {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': "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"
-        },
-      })
-      .then(response => response.json())
-      .then(data => setData(data))
-      
-      
-; 
-     }, [createdAtState]);
-  
- 
-    const loopDates = async () => {
-     
-      
-      const createdAt = [];
-      const gists = [];
-      for (let i = 0; i < data.length; i+=5) {
-        createdAt.push(data[i].created_at);
-      }
+  useEffect(() => {
+    fetch("https://api.github.com/gists?per_page=25", {
+      headers: {
+        Authorization: "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",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, [createdAtState]);
 
+  const loopDates = async () => {
+    const createdAt = [];
+    const gists = [];
+    for (let i = 0; i < data.length; i += 5) {
+      createdAt.push(data[i].created_at);
+    }
 
+    // for (let i = 0; i < data.length; i++) {
+    //   gists.push(data.length);
+    // }
+    setCreatedAtState(createdAt);
+    setNumberOfGists(gists);
+    console.log(numberOfGists);
+  };
 
-      // for (let i = 0; i < data.length; i++) {
-      //   gists.push(data.length);
-      // }
-      setCreatedAtState(createdAt);
-      setNumberOfGists(gists)
-      console.log(numberOfGists);
-    };
+  console.log(createdAtState);
 
-    console.log(createdAtState);
-  
-  
-  
+  const chartData = {
+    labels: [
+      createdAtState[0],
+      createdAtState[1],
+      createdAtState[2],
+      createdAtState[3],
+    ],
+    datasets: [
+      {
+        data: [50, 100, 200, 300, 400, 500],
+        label: "Gists Created",
+      },
+    ],
+  };
 
+  const chartData2 = {
+    labels: [
+      createdAtState[0],
+      createdAtState[1],
+      createdAtState[2],
+      createdAtState[3],
+    ],
+    datasets: [
+      {
+        data: [50, 100, 200, 300, 400, 500],
+        label: "Files Per Gist",
+      },
+    ],
+  };
 
-const chartData = {
-  labels: [
-    createdAtState[0],
-    createdAtState[1],
-    createdAtState[2],
-    createdAtState[3],
-    
-  ],
-  datasets: [
-    {
-      data: [50, 100, 200, 300, 400, 500],
-      label: "Gists Created"
-    },
-    
-  ],
-
-  
-};
-
-const options={
-      title: {
+  const options = {
+    title: {
       display: true,
-      text: 'World population per region (in millions)'
-    
-  }
-}
+      text: "World population per region (in millions)",
+    },
+  };
 
-
+  const options2 = {
+    title: {
+      display: true,
+      text: "World population per region (in millions)",
+    },
+  };
 
   return (
     <div className="container">
       <div className="mainTitle">Notepad Application </div>
       <div className="card-gists">
+        {" "}
+        <div className="gist-chart">
+          <Line data={chartData} options={options} />
+        </div>
         <div className="stats-button">
           <Link to="/">
             <button className="close-stats-btn">Close Stats</button>
           </Link>
         </div>
-        <div className="gist-chart">
-          <Line data={chartData} options={options} />
-        </div>
         <div className="load-btn">
-          <button className="load-more-btn" onClick={loopDates}>Load More</button>
+          <button className="load-more-btn" onClick={loopDates}>
+            Load More
+          </button>
         </div>
         <div className="gist-chart-files">
-          <Line data={chartData} options={options} />
+          <Line data={chartData2} options={options2} />
+        </div>
+        <div className="load-btn-2">
+          <button className="load-more-btn-2" onClick={loopDates}>
+            Load More
+          </button>
         </div>
       </div>
     </div>
--- client/src/pages/Notepad.js
+++ client/src/pages/Notepad.js
@@ -1,202 +1,157 @@
 import React, { useEffect, useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "../styles/notepad.css";
 
 const Notepad = () => {
-  
-    const [notepadTitle, setNotepadTitle] = useState();
-    const [persistnotePadTitle, setPersistnotePadTitle] = useState("")
-    const [noteTitle, setNoteTitle] = useState("");
-    const [persistNoteTitle, setPersistNoteTitle] = useState([])
-    const [note, setNote] = useState("");
-    const [persistNote, setPersistNote] = useState([]);
-    const [localNotepadTitle, setLocalNotepadTitle] = useState("");
+  const [notepadTitle, setNotepadTitle] = useState();
+  const [persistnotePadTitle, setPersistnotePadTitle] = useState("");
+  const [noteTitle, setNoteTitle] = useState("");
+  const [persistNoteTitle, setPersistNoteTitle] = useState([]);
+  const [note, setNote] = useState("");
+  const [persistNote, setPersistNote] = useState([]);
+  const [localNotepadTitle, setLocalNotepadTitle] = useState("");
 
+  useEffect(() => {
+    const theme = localStorage.getItem("notepadTitle") === "true";
+    const parsedData = JSON.parse(theme);
+    console.log(parsedData);
+  }, []);
 
-//   useEffect(() => {
-//        const theme = localStorage.getItem('notepadTitle') === "true";
-//     const parsedData = JSON.parse(theme);
-//           console.log(parsedData)
-//   }, []);
-    
-  
-    
-    
-    // console.log(persistNoteTitle);
-    console.log(localNotepadTitle);
-   
+  useEffect(() => {
+    const setTitle = () => {
+      setLocalNotepadTitle(localStorage.getItem("notepadTitle"));
+      setTitle();
+    };
+  }, []);
 
+  const submitNotepadTitle = async (event) => {
+    event.preventDefault();
+    await setPersistnotePadTitle(notepadTitle);
+    await localStorage.setItem("notepadTitle", notepadTitle);
 
+    await setNotepadTitle("");
+  };
 
-    const submitNotepadTitle = async (event) => {
-        event.preventDefault();
-        await setPersistnotePadTitle(notepadTitle)
-        await  localStorage.setItem("notepadTitle", notepadTitle);
-        
-        await setNotepadTitle("");
+  const submitNote = async (event) => {
+    event.preventDefault();
 
+    await setPersistNoteTitle([...persistNoteTitle, noteTitle]);
+    await setPersistNote([...persistNote, note]);
+    await localStorage.setItem("noteTitle", noteTitle);
+    await localStorage.setItem("note", note);
 
-      }
- 
-      const submitNote = async (event) => {
-          event.preventDefault();
-         
-          await setPersistNoteTitle([...persistNoteTitle, noteTitle]);
-          await setPersistNote([...persistNote,  note]);
-          await localStorage.setItem("noteTitle", noteTitle);
-          await localStorage.setItem("note", note);
+    await setNoteTitle("");
+    await setNote("");
+  };
 
-          await setNoteTitle("");
-          await setNote("");
+  const deleteArray = async () => {
+    await setPersistNote("");
+    await setPersistNoteTitle("");
+    await localStorage.setItem("noteTitle", "");
+    await localStorage.setItem("note", "");
+  };
 
-      }
-    
-    const deleteArray = async () => {
-      
-        await setPersistNote("");
-         setPersistNoteTitle("");
-    }
-
-
-  
- useEffect(()=>{
-const setTitle = ()=> {
-  setLocalNotepadTitle(localStorage.getItem("notepadTitle"));
-  setTitle();
-}},[]);
-
-console.log(localNotepadTitle)
-
-
+  console.log(localNotepadTitle);
 
   // useEffect(()=>{
   //     localStorage.setItem('notepadTitle', persistnotePadTitle)
   // },[persistnotePadTitle]);
 
- 
+  return (
+    <div className="container">
+      <div className="mainTitle">Notepad Application </div>
+      <div className="card">
+        <div className="notepad-title">
+          <p>Notepad Title </p>
+          <br />
 
+          <input
+            className="input-title"
+            maxLength={255}
+            placeholder="My notepad title..."
+            value={notepadTitle}
+            onChange={(e) => setNotepadTitle(e.target.value)}
+          />
+        </div>
+        <div className="title-btns">
+          <Link to="/gists">
+            <button className="stats-btn">View Stats</button>
+          </Link>
 
+          <button className="save-btn" onClick={submitNotepadTitle}>
+            Save
+          </button>
+          <button
+            className="del-btn"
+            onClick={() => setPersistnotePadTitle("")}
+          >
+            Delete
+          </button>
+        </div>
+        <div className="note-title">
+          {persistnotePadTitle ? (
+            <h3>{localStorage.getItem("notepadTitle")}</h3>
+          ) : (
+            <h3>My Notes</h3>
+          )}
+          <br />
+          <input
+            className="input-title"
+            placeholder="Enter Note Title..."
+            value={noteTitle}
+            onChange={(e) => setNoteTitle(e.target.value)}
+          />
+        </div>
+        <div className="note">
+          <input
+            className="input-note"
+            maxLength={1000}
+            placeholder="Enter Note..."
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+          />
+        </div>
+        <div className="add-btn">
+          <button className="add-button" onClick={submitNote}>
+            Add
+          </button>
+        </div>
 
-    return (
-      <div className="container">
-        <div className="mainTitle">Notepad Application </div>
-        <div className="card">
-          <div className="notepad-title">
-            <p>Notepad Title </p>
-            <br />
-
+        <div className="first-note-title">
+          {persistNoteTitle ? (
             <input
               className="input-title"
-              maxLength={255}
-              placeholder="My notepad title..."
-              value={notepadTitle}
-              onChange={(e) => setNotepadTitle(e.target.value)}
+              value={[localStorage.getItem("noteTitle")]}
+              readOnly
             />
-          </div>
-          <div className="title-btns">
-            <Link to="/gists">
-            
-            <button className="stats-btn">View Stats</button></Link>
-            
-            
-            <button className="save-btn" onClick={submitNotepadTitle}>
-              Save
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="delete-btn">
+          {persistNoteTitle ? (
+            <button className="del-btn" onClick={deleteArray}>
+              Delete
             </button>
-            <button className="del-btn" onClick={() => setPersistnotePadTitle("")}>Delete</button>
-          </div>
-          <div className="note-title">
-            {persistnotePadTitle ? persistnotePadTitle : <h3>My Notes</h3>}
-            <br />
-            <input
-              className="input-title"
-              placeholder="Enter Note Title..."
-              value={noteTitle}
-              onChange={(e) => setNoteTitle(e.target.value)}
-            />
-          </div>
-          <div className="note">
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="first-note">
+          {persistNoteTitle ? (
             <input
               className="input-note"
-              placeholder="Enter Note..."
-              value={note}
-              onChange={(e) => setNote(e.target.value)}
+              value={[localStorage.getItem("note")]}
+              readOnly
             />
-          </div>
-          <div className="add-btn">
-            <button className="add-button" onClick={submitNote}>
-              Add
-            </button>
-          </div>
-
-          <div className="first-note-title">
-            {persistNoteTitle  ? (
-              <input className="input-title" value={persistNoteTitle} />
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="delete-btn">
-            {persistNoteTitle ? (
-              <button className="del-btn" onClick={deleteArray}>Delete</button>
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="first-note">
-            {persistNoteTitle ? (
-              <input className="input-note" value={persistNote} />
-            ) : (
-              ""
-            )}
-          </div>
-
-          {/* <div className="notepad-title-input">
-            Notepad Title <br />
-                    <input placeholder="My notepad title..."
-                        onChange={(e) =>
-                setData({ notepadTitle: e.target.value })
-              }></input>
-          </div>
-
-          <div className="view-stats">
-            <button className="view-stats-btn">View Stats</button>
-          </div>
-
-          <div className="save">
-                    <button
-                        
-                        type="submit"
-                        onClick={handleSubmit}
-              className="save-btn"
-             
-            >
-              Save
-            </button>
-          </div>
-
-          <div className="delete">
-            <button className="delete-btn">Delete</button>
-          </div>
-
-          <div className="notes-heading">
-            {localStorage.getItem("noteTitle")}
-            <br />
-            <input
-              className="note-title-input"
-              placeholder="Enter note title..."
-            ></input>
-            <br />
-            <input className="note-input" placeholder="Enter note..."></input>
-          </div>
-
-          <div className="add">
-            <button className="add-btn">Add</button>
-          </div> */}
+          ) : (
+            ""
+          )}
         </div>
       </div>
-    );
-    
-}
+    </div>
+  );
+};
 
 export default Notepad;
